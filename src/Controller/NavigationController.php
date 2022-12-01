70381c691518d6b5607d5dab675a46ef826b4a13
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2022-12-01 09:17:06 +0100

    rick

--- src/Controller/NavigationController.php
+++ src/Controller/NavigationController.php
@@ -4,66 +4,35 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+* @IsGranted("ROLE_ADMIN")
+*/
 class NavigationController extends AbstractController
 {
         /**
          * @Route("/", name="home")
          */
-        public function home(Session $session)
+        public function home()
         {
-                $return = [];
-
-                if($session->has('message'))
-                {
-                        $message = $session->get('message');
-                        $session->remove('message'); //on vide la variable message dans la session
-                        $return['message'] = $message; //on ajoute à l'array de paramètres notre message
-                }
-                return $this->render('navigation/home.html.twig', $return);
+                return $this->render('navigation/home.html.twig');
         }
 
         /**
          * @Route("/membre", name="membre")
          */
-        public function membre(Session $session)
+        public function membre()
         {
-                $return = [];
-
-                if($session->has('message'))
-                {
-                        $message = $session->get('message');
-                        $session->remove('message'); //on vide la variable message dans la session
-                        $return['message'] = $message; //on ajoute à l'array de paramètres notre message
-                }
-                return $this->render('navigation/membre.html.twig', $return);
+                return $this->render('navigation/membre.html.twig');
         }
 
         /**
          * @Route("/admin", name="admin")
          */
-        public function admin(Session $session)
+        public function admin()
         {
-                $User = $this->getUser();
-                if(!$User)
-                {
-                        $session->set("message", "Merci de vous connecter");
-                        return $this->redirectToRoute('app_login');
-                }
-
-                else if(in_array('ROLE_ADMIN', $User->getRoles())){
-                        return $this->render('navigation/admin.html.twig');
-                }
-                $session->set("message", "Vous n'avez pas le droit d'acceder à la page admin vous avez été redirigé sur cette page");
-                if($session->has('message'))
-                {
-                        $message = $session->get('message');
-                        $session->remove('message'); //on vide la variable message dans la session
-                        $return['message'] = $message; //on ajoute à l'array de paramètres notre message
-                }
-                return $this->redirectToRoute('home', $return);
-
+                return $this->render('navigation/admin.html.twig');
         }
 
 }
