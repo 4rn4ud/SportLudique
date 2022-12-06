c2b1623a9f7dc1a18f820ee4fe579b2f7aeea318
4rn4ud <arnaud.lechat1@gmail.com>
2022-12-06 15:48:59 +0100

    routes

--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -8,9 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/single')]
 class SingleController extends AbstractController
 {
-    #[Route('/{id}', name: 'single', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_single_index', methods: ['GET'])]
     public function index(Product $product, ProductRepository $productRepository): Response
     {
         return $this->render('single.html.twig', [
