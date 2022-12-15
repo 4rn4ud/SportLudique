67b67f299753547c5fe5ef3fc1ddba9ff49c1774
4rn4ud <arnaud.lechat1@gmail.com>
2022-12-15 13:34:27 +0100

    changes

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -12,11 +13,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'shop')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, Request $request): Response
     {
-        return $this->render('shop.html.twig', [
-            'controller_name' => 'ShopController',
-            'products' => $productRepository->findAll(),
-        ]);
+        //$product = new Product();
+        
+        if ($request->isMethod('GET')) 
+        {
+            if($request->get('search') == null)
+            {
+                return $this->render('shop.html.twig', [
+                    'controller_name' => 'ShopController',
+                    'products' => $productRepository->findAll(),
+                ]);
+            }
+            $recherche = $request->get('search');
+            $product = $productRepository->findOneBy(['label' => $recherche]);
+            
+            
+            return $this->render('shop.html.twig', 
+            [
+                'controller_name' => 'ShopController',
+                'products' => $product,
+            ]);
+        }
+        
+        
     }
 }
