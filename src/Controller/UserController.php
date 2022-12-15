3b89c4c82a06af86c01b8904692146849eb2ff6e
Titouan Clapier <58605712+TitouanClapier@users.noreply.github.com>
2022-12-15 08:56:07 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,19 +14,12 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[IsGranted('ROLE_ADMIN'),IsGranted('ROLE_USER')]
+
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    #[Route('/', name: 'utilisateur_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+
 
     #[Route('/register', name: 'register', methods: ['GET', 'POST'])]
     public function new(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
@@ -53,7 +46,18 @@ class UserController extends AbstractController
             'form' => $form,
         ]);
     }
+    
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    #[Route('/', name: 'utilisateur_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
 
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
@@ -62,7 +66,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
 
     public function edit(Request $request, User $user, UserRepository $userRepository): Response
@@ -82,6 +86,7 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
